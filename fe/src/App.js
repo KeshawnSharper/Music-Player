a32575e53c59095fa17447881be48e7deeb49ded
KeshawnSharper <ksharper@studentmba.org>
2021-07-03 11:17:05 -0400

    error check on login page

--- fe/src/App.js
+++ fe/src/App.js
@@ -6,39 +6,26 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Login from "./Components/Authenication/Login/Login"
 function App() {
   return (
     <div className="App">
       <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+  
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
+          {/* <Route path="/about">
             <About />
           </Route>
           <Route path="/users">
             <Users />
-          </Route>
+          </Route> */}
           <Route path="/">
-            <Home />
+            <Login />
           </Route>
         </Switch>
-      </div>
     </Router>
     </div>
   );
