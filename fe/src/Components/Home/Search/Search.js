53576ee7a5be8dcd24dbe1733d2c000025e7ebe5
KeshawnSharper <ksharper@studentmba.org>
2021-07-17 14:32:51 -0400

    Search functionality and design

--- fe/src/Components/Home/Search/Search.js
+++ fe/src/Components/Home/Search/Search.js
@@ -1,17 +1,27 @@
 import React,{useState} from "react"
-
+import "./Search.css"
 const Search = props => {
     const {handleChange,value} = props
 return (
 <>
 <form>
-    <input value={value.name} name="name" onChange={e => handleChange(e)} />
-    <select id="cars" name="type" onChange={e => handleChange(e)}>
+<div class="searchBox">
+
+
+<input class="searchInput"type="text" placeholder="Search" value={value.name} name="name" onChange={e => handleChange(e)} />
+<button class="searchButton" href="#">
+<i class="fa fa-search" aria-hidden="true"></i>
+</button>
+<select class="searchInput" name="type" onChange={e => handleChange(e)}>
     <option value="song">Song</option>
     <option value="artist">Artist</option>
     <option value="album">Album</option>
     <option value="playlist">Playlist</option>
   </select>
+</div>
+
+
+   
 </form>
 </>
 )
