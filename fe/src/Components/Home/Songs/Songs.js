664442690313af2773f560f991c5dcc26f8a6e05
KeshawnSharper <ksharper@studentmba.org>
2021-07-18 23:33:20 -0400

    save

--- fe/src/Components/Home/Songs/Songs.js
+++ fe/src/Components/Home/Songs/Songs.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import Song from "./Song";
+import Song from "./Song"
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
